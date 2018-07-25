eab1342ac14acf2918630fda3c531e6a2895b315
romchambe <chamberomain@gmail.com>
2018-07-25 14:35:30 +0200

    pre init new branch

--- components/NotesIndex.js
+++ components/NotesIndex.js
@@ -9,9 +9,8 @@ class NotesIndex extends React.Component {
   render () {
 
     return (
-
       <View >
-
+        
         <Text>
           Scan a book, a screen, a graffiti with your phone, extract the text and keep note of it!
         </Text>
